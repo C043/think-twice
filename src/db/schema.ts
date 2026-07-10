cfb98cfd4a651b28bd8fe523a41bdc21ecffb330
C043 <mariofragnitoph@gmail.com>
2026-07-10 09:54:28 +0200

    Adds notified column to objects table, also changed docker-compose.dev.yml pgadmin settings

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, uuid, text, integer, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  integer,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
 
 export const objectsTable = pgTable("objects", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -6,6 +13,7 @@ export const objectsTable = pgTable("objects", {
   price: integer("price").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   reviewAt: timestamp("review_at").notNull(),
+  notified: boolean("notified").default(false).notNull(),
 });
 
 export type InsertObject = typeof objectsTable.$inferInsert;
